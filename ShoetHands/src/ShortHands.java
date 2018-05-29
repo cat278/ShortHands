9b54ee709365dd2493289ad80bc786c1607929d7
divyag <divyag@10.11.2.149>
2018-05-29 18:29:12 +0530

    Short Hands commit all.

---
+++ ShoetHands/src/ShortHands.java
@@ -0,0 +1,25 @@
+
+public class ShortHands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5;
+		//System.out.println(i++);
+		System.out.println(i);
+
+		i*=2;
+		System.out.println(i);
+		i=5;
+		i+=2;
+		System.out.println(i);
+		i=5;
+		i-=2;
+		System.out.println(i);
+		
+		byte b=8;
+		b*=2.5;
+		System.out.println((double)b);
+		
+	}
+
+}
